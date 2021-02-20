9fc3d1d98256e19bf0f331f0bdf97b0e25bdea79
Sho Maruyama <mrsho34@gmail.com>
2021-02-20 16:01:24 +0900

    add: asyncSumOfArray test

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,7 +1,13 @@
 // todo: ここに単体テストを書いてみましょう！
 
-import { sumOfArray } from "./../functions";
+import { sumOfArray, asyncSumOfArray } from "./../functions";
 
-test('adds 1 + 2 to equal 3', () => {
+test('sum: adds 1 + 2 to equal 3', () => {
   expect(sumOfArray([1, 2])).toBe(3);
+});
+
+test('async: adds 1 + 2 to equal 3', () => {
+  return asyncSumOfArray([1, 2]).then(data => {
+    expect(data).toBe(3);
+  });
 });
