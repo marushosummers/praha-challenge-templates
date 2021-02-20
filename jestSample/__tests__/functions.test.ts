1c02cda8aa862ecf412e5d0ca6b6b560f8781786
Sho Maruyama <mrsho34@gmail.com>
2021-02-20 16:53:58 +0900

    sumOfArrayとasyncSumOfArrayのテストを作成

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -2,12 +2,50 @@
 
 import { sumOfArray, asyncSumOfArray } from "./../functions";
 
-test('sum: adds 1 + 2 to equal 3', () => {
-  expect(sumOfArray([1, 2])).toBe(3);
+describe("sumOfArray", (): void => {
+
+    test('1+2が3になる', () => {
+        expect(sumOfArray([1, 2])).toBe(3);
+    });
+
+    test('-1+1が0になる', () => {
+        expect(sumOfArray([-1, 1])).toBe(0);
+    });
+
+    test('空のArrayはErrorになる', () => {
+        expect(() => {
+            sumOfArray([]);
+        }).toThrow(TypeError);
+    });
+
+    test('1はそのまま1', () => {
+        expect(sumOfArray([1])).toBe(1);
+    });
+
+    test('1+2+3は6になる', () => {
+        expect(sumOfArray([1,2,3])).toBe(6);
+    });
 });
 
-test('async: adds 1 + 2 to equal 3', () => {
-  return asyncSumOfArray([1, 2]).then(data => {
-    expect(data).toBe(3);
-  });
+describe("asyncSumOfArray", (): void => {
+
+    test('1+2が3になる', () => {
+        asyncSumOfArray([1, 2]).then(data => {
+            expect(data).toBe(3);
+        });
+    });
+
+    test('-1+1が0になる', () => {
+        asyncSumOfArray([-1, 1]).then(data => {
+            expect(data).toBe(0);
+        });
+    });
+
+    test('空のArrayはErrorになる', () => {
+        asyncSumOfArray([]).then(data => {
+            expect(data).toThrow(TypeError);
+        });
+        });
+
+
 });
